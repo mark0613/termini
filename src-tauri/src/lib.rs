e01fa0302a3d8d4f779aa23f5696be800258e012
Mark Ma <goldffd700@gmail.com>
2026-06-07 20:12:22 +0800

    feat: persist main window state

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,10 +12,18 @@ mod storage;
 
 use state::AppState;
 use tauri::Manager;
+use tauri_plugin_window_state::{Builder as WindowStateBuilder, StateFlags};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(
+            WindowStateBuilder::new()
+                .with_state_flags(
+                    StateFlags::SIZE | StateFlags::MAXIMIZED | StateFlags::FULLSCREEN,
+                )
+                .build(),
+        )
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
